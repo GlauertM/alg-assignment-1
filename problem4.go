a29ea28a5ca213e9d199c651135b949fdf937429
GlauertM <gratia19@gmail.com>
2025-10-15 00:34:40 +0300

    Changed problem4.go using 3rd pointer

--- problem4.go
+++ problem4.go
@@ -3,35 +3,18 @@ package main
 import "fmt"
 
 func merge_sorted_arrays(arr1, arr2 []int) []int {
-	// merged_array := []int{}
-	// i, j := 0, 0
-
-	// for i < len(arr1) && j < len(arr2) {
-	// 	if arr1[i] < arr2[j] {
-	// 		merged_array.append(merged_array, arr1[i])
-	// 		i++
-	// 	} else {
-	// 		merged_array.append(merged_array, arr2[j])
-	// 		j++
-	// 	}
-	// }
-
-	// merged_array.append(merged_array, arr1[i])
-	// merged_arrayappend(merged_array, arr2[j])
-
-	// return merged_array
-
-	pntr_arr2 := len(arr2) - 1
-	last_pntr_arr1 := len(arr1) - 1
-
-	for pntr_arr1 := len(arr1) - 1; pntr_arr2 >= 0; last_pntr_arr1-- {
-		if pntr_arr1 >= 0 && arr2[pntr_arr2] < arr1[pntr_arr1] {
-			arr1[last_pntr_arr1] = arr1[pntr_arr1]
-			pntr_arr1--
+	pntr1 := len(arr1) - len(arr2) - 1
+	pntr2 := len(arr2) - 1
+	pntrMid := len(arr1) - 1
+	for pntr2 >= 0 {
+		if pntr1 >= 0 && arr1[pntr1] < arr2[pntr2] {
+			arr1[pntrMid] = arr1[pntr1]
+			pntr1--
 		} else {
-			arr1[last_pntr_arr1] = arr2[pntr_arr2]
-			pntr_arr2--
+			arr1[pntrMid] = arr2[pntr2]
+			pntr2--
 		}
+		pntrMid--
 	}
 	return arr1
 }
