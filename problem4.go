3a3d192d1ef4fb78de51949d4bc764f3ee15d714
GlauertM <gratia19@gmail.com>
2025-10-19 00:37:43 +0300

    Changed problem4.go using 3rd pointer

--- problem4.go
+++ problem4.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func merge_sorted_arrays(arr1, arr2 []int) []int {
 	pntr1 := len(arr1) - len(arr2) - 1
 	pntr2 := len(arr2) - 1
@@ -19,8 +17,8 @@ func merge_sorted_arrays(arr1, arr2 []int) []int {
 	return arr1
 }
 
-func main() {
-	sorted_array1 := []int{1, 2, 3, 6, 7, 8, 10, 11, 12, 16, 22, 0, 0, 0, 0}
-	sorted_array2 := []int{4, 5, 9, 13}
-	fmt.Println(merge_sorted_arrays(sorted_array1, sorted_array2))
-}
+// func main() {
+// 	sorted_array1 := []int{1, 2, 3, 6, 7, 8, 10, 11, 12, 16, 22, 0, 0, 0, 0}
+// 	sorted_array2 := []int{4, 5, 9, 13}
+// 	fmt.Println(merge_sorted_arrays(sorted_array1, sorted_array2))
+// }
