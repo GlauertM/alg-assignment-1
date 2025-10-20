aee08e041b0c9eb4b6b8d994cd6a5ef75fdd116c
GlauertM <gratia19@gmail.com>
2025-10-20 21:11:32 +0300

    Fixed []int{0} case.

--- problem8.go
+++ problem8.go
@@ -1,18 +1,13 @@
 package main
 
+import "fmt"
+
 func zeroLast(arr []int) []int {
 	pntr_l := 0
 	pntr_r := len(arr) - 1
-	// for pntr_l <= pntr_r {
-	// 	if arr[pntr_l] == 0 {
-	// 		arr[pntr_l], arr[pntr_r] = arr[pntr_r], arr[pntr_l]
-	// 		pntr_r--
-	// 		if arr[pntr_l] == 0 {
-	// 			pntr_l--
-	// 		}
-	// 	}
-	// 	pntr_l++
-	// }
+	if len(arr) == 1 {
+		return arr
+	}
 
 	for pntr_l <= pntr_r {
 		if arr[pntr_r] == 0 {
@@ -28,7 +23,7 @@ func zeroLast(arr []int) []int {
 	return arr
 }
 
-// func main() {
-// 	sorted_array := []int{0, 0, 1, 2, 0, 6, 0, 8, 10, 0, 12, 16, 22}
-// 	fmt.Println(zeroLast(sorted_array))
-// }
+func main() {
+	sorted_array := []int{0, 0}
+	fmt.Println(zeroLast(sorted_array))
+}
